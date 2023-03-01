bceb7fe1dd22ecdcebdba67190db14746fefc65e
Adeyemi Isaac <adeyemiisaac@gmail.com>
2023-03-01 15:13:00 +0000

    Task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,21 +15,19 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+binary_tree_t *new;
+if (parent == NULL)
+return (NULL);
+new = malloc(sizeof(binary_tree_t));
+if (new == NULL)
+return (NULL);
+new->n = value;
+new->parent = parent;
+new->left = NULL;
+new->right = parent->right;
+parent->right = new;
+if (new->right)
+new->right->parent = new;
 
-	if (parent == NULL)
-		return (NULL);
-
-	new = binary_tree_node(parent, value);
-	if (new == NULL)
-		return (NULL);
-
-	if (parent->right != NULL)
-	{
-		new->right = parent->right;
-		parent->right->parent = new;
-	}
-	parent->right = new;
-
-	return (new);
+return (new);
 }
